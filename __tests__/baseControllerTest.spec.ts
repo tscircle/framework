74d0c52e25c2cf2d169d5c9c8ffc16e97a58d6dc
henriklippke <h.lippke@exporo.com>
2019-12-09 14:41:16 +0100

    hotfix/addMoreTests

---
+++ __tests__/baseControllerTest.spec.ts
@@ -0,0 +1,47 @@
+import {expect} from 'chai';
+import 'mocha';
+import * as request from 'supertest';
+import {EmailSpecialController} from "../application/domain/email/controllers/emailSpecialController";
+import {EmailSpecialPostController} from "../application/domain/email/controllers/emailSpecialPostController";
+
+
+describe('Base Controller Tests', () => {
+    it('should respond a predefined response', async () => {
+        const ctr = new EmailSpecialController();
+        const app = ctr.setupAPIHandler();
+
+        const response = await request(app)
+            .get("/email/1/special")
+            .expect(200);
+
+        const data = JSON.parse(response.text);
+        expect(data.hello).to.eql("from EmailSpecialController");
+
+    });
+
+    it('should respond a validation error message', async () => {
+        const ctr = new EmailSpecialPostController();
+        const app = ctr.setupAPIHandler();
+
+        const response = await request(app)
+            .post("/email/1/special")
+            .expect(422);
+
+        const data = JSON.parse(response.text);
+        expect(data[0].message).to.eql('"data" is required');
+    });
+
+    it('should respond a successful response', async () => {
+        const ctr = new EmailSpecialPostController();
+        const app = ctr.setupAPIHandler();
+
+        const response = await request(app)
+            .post("/email/1/special")
+            .send({name: 'henrik'})
+            .expect(200);
+
+        const data = JSON.parse(response.text);
+        expect(data[0].message).to.eql('"data" is required');
+    });
+});
+
